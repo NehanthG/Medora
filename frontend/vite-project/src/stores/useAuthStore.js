20fa886f0d390eb159432431669ab863b51122b1
Surya <phxntomcodm@gmail.com>
2025-12-25 22:05:52 +0530

    fixed small errors

--- frontend/vite-project/src/stores/useAuthStore.js
+++ frontend/vite-project/src/stores/useAuthStore.js
@@ -22,6 +22,7 @@ export const useAuthStore = create((set) => ({
       });
       set({ authUser: res.data });
     } catch (error) {
+      console.log(error);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false }); // ✅ THIS IS ENOUGH
